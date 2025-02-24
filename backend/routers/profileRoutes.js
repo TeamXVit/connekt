2b80a77d8e26505639b00927d7a45b81c1a5654f
Ganesh M <blackboard717@gmail.com>
2025-02-24 23:48:07 +0530

    added more endpoints to profile router.

--- backend/routers/profileRoutes.js
+++ backend/routers/profileRoutes.js
@@ -10,7 +10,11 @@ const storage = multer.memoryStorage();
 const upload = multer({storage});
 const profileRouter = express.Router();
 
-profileRouter.post("/upload-profilepicture",authenticateToken, upload.single("image"), async (request, response)=>{
+profileRouter.post(
+    "/upload-profilepicture",
+    authenticateToken, 
+    upload.single("image"), 
+    async (request, response)=>{
     try{
         const { regno } = request.user;
         if(!request.file) return response.status(400).send({
@@ -38,6 +42,79 @@ profileRouter.post("/upload-profilepicture",authenticateToken, upload.single("im
         return response.status(200).send({
             message: "Profile picture uploaded successfully."
         });
+    }catch(err){
+        return response.status(500).send({
+            error : `Internal Server Error : ${err.message}`
+        });
+    }
+});
+
+profileRouter.get("/view/:regno", authenticateToken, async (request, response)=>{
+    try{
+        const { regno } = request.params;
+        const user = await Users.findOne({regno:regno.toUpperCase()});
+        if(!user) return response.status(400).send({
+            error: "User not Found"
+        });
+        const data = {
+            name: user.name,
+            regno: user.regno,
+            gender: user.gender,
+            profilepicture: user.profilepicture,
+            email: user.email,
+            instagram: user.instagram
+        };
+        return response.status(200).send(data);
+    }catch(err){
+        return response.status(500).send({
+            error : `Internal Server Error : ${err.message}`
+        });
+    }
+});
+
+profileRouter.get("/view",authenticateToken, async (request, response)=>{
+    try{
+        const { regno } = request.user;
+        const user = await Users.findOne({regno});
+        if(!user) return response.status(404).send({
+            error: "User not Found"
+        });
+        const data = {
+            name: user.name,
+            regno: user.regno,
+            gender: user.gender,
+            profilepicture: user.profilepicture,
+            email: user.email,
+            instagram: user.instagram,
+            dob: user.dob,
+            phoneno: user.phoneno
+        };
+        return response.status(200).send(data);
+    }catch(err){
+        return response.status(500).send({
+            error : `Internal Server Error : ${err.message}`
+        });
+    }
+})
+
+profileRouter.patch("/edit",authenticateToken, async (request, response)=>{
+    try{
+        const { name, phoneno, instagram } = request.body;
+        if (!name && !phoneno && !instagram) {
+            return res.status(400).json({ error: "At least one field must be provided for update." });
+        }
+        const { regno } = request.user;
+        const user = await Users.findOne({regno});
+        if(!user) return response.status(404).send({
+            error: "User not Found"
+        });
+        if(name) user.name = name;
+        if(phoneno) user.phoneno = phoneno;
+        if(instagram) user.instagram = instagram;
+        await user.save();
+        return response.status(200).send({
+            message : "Profile edited successfully"
+        }); 
     }catch(err){
         return response.status(500).send({
             error : `Internal Server Error : ${err.message}`
